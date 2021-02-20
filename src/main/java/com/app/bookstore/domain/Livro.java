a73d0a6495492be98e09771a5d90a884e9336102
jowcarvajal <jowcarvajal@gmail.com>
2021-02-20 16:51:57 -0300

    commit-domain-annotation

--- src/main/java/com/app/bookstore/domain/Livro.java
+++ src/main/java/com/app/bookstore/domain/Livro.java
@@ -2,16 +2,28 @@ package com.app.bookstore.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Livro implements Serializable {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String titulo;
 	private String autor;
 	private String descricao;
+	@ManyToOne
+	@JoinColumn(name = "categoria_id")
 	private Categoria categoria;
 
 	public Livro() {
